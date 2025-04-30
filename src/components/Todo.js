1f5ce17b32aee0ec1d3cf8b769b1390355833f87
Shihab <shihab96056789@gmail.com>
2025-04-30 23:09:06 +0530

    added the 3 buttons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Todo.css";
+import { IoMdDoneAll } from "react-icons/io";
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
 
 const Todo = () => {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+
   const addTodo = () => {
-    setTodos([...todos, todo]);
+    const newTodo = {
+      id: Date.now(),
+      text: todo,
+    };
+    setTodos([...todos, newTodo]);
     setTodo("");
   };
   const handleSubmit = (e) => {
     e.preventDefault();
   };
-  const inputRef = useRef('null');
-  useEffect(()=>{
+  const inputRef = useRef("null");
+  useEffect(() => {
     inputRef.current.focus();
-  })
+  });
   return (
     <div className="container">
       <h2>Todo App</h2>
@@ -31,11 +39,24 @@ const Todo = () => {
       </form>
       <div className="list">
         <ul>
-       {
-        todos.map((todo)=>(
-            <li>{todo}</li>
-        ))
-       }
+          {todos.map((todo) => (
+            <li className="list-items" key={todo.id}>
+              <div className="list-item-list">{todo.text}</div>
+              <span>
+                <IoMdDoneAll
+                  className="list-item-icons"
+                  id="complete"
+                  title="Complete"
+                />
+                <FiEdit className="list-item-icons" id="edit" title="Edit" />
+                <MdDelete
+                  className="list-item-icons"
+                  id="delete"
+                  title="Delete"
+                />
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
