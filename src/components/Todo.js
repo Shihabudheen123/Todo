e28a7348764769eca92e09ce36dbe6800a9e5967
Shihab <shihab96056789@gmail.com>
2025-04-30 21:53:55 +0530

    take inputs and show in the display list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,24 +1,45 @@
-import React from 'react'
-import './Todo.css'
+import React, { useEffect, useRef, useState } from "react";
+import "./Todo.css";
 
-const Todo = ()=>{
-    return (
-        <div className='container'>
-            <h2>Todo App</h2>
-            <form className='form-group'>
-                <input type="text" placeholder="Add a new task" className='form-control' />
-                <button>ADD</button>
+const Todo = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo("");
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const inputRef = useRef('null');
+  useEffect(()=>{
+    inputRef.current.focus();
+  })
+  return (
+    <div className="container">
+      <h2>Todo App</h2>
+      <form className="form-group" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          ref={inputRef}
+          placeholder="Add a new task"
+          className="form-control"
+          onChange={(event) => setTodo(event.target.value)}
+        />
+        <button onClick={addTodo}>ADD</button>
+      </form>
+      <div className="list">
+        <ul>
+       {
+        todos.map((todo)=>(
+            <li>{todo}</li>
+        ))
+       }
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-            </form>
-            <div className='list'>
-            <ul>
-                <li className='form-control'>Something</li>
-                <li className='form-control'>Something</li>
-                <li className='form-control'>Something</li>
-            </ul>
-            </div>
-        </div>
-    )
-}
-
-export default Todo
+export default Todo;
